f59ffec6bbd9469bfcdccc66f963d3d71396791f
Daniel Jauregui <djvdjv209@gmail.com>
2022-08-30 22:12:20 -0700

    Added navBar component and pages for Home and a 404 page

---
+++ client/src/pages/NoMatch/noMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import noMatchStyles from "./noMatch.css";
+import Home from "../Home/home.js"
+
+function NoMatch() {
+    return (<div>
+        <h1>No Page Found :( Please go to <a>Home</a></h1>
+    </div>)
+}
+
+export default Nomatch; 
